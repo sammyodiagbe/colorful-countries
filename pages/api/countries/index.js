64fb9e9274b086d49a5199077472dd5f1f110633
Odiagbe Samson Osaro <odiagbesamsonosaro@gmail.com>
2021-08-02 03:03:30 +0100

    adding api end point to next app

---
+++ pages/api/countries/index.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://restcountries.eu");
+    const data = await response.json();
+    return res.status(200).json({
+      coutries: data,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: "Something went wrong",
+    });
+  }
+}
